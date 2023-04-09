47d22cc36893c97e47099e3f94e2e872985dc69c
Frédéric Mheir <63975740+fredmheir@users.noreply.github.com>
2023-04-09 17:25:27 -0400

    added hero component

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Computers = () => {
   return (
-    <div>Computers</div>
+    <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>Computers</div>
   )
 }
 
