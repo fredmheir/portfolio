b1a91d206ec68d2579f84e3770a78c92b60dcab6
Frédéric Mheir <63975740+fredmheir@users.noreply.github.com>
2023-04-21 00:40:40 -0400

    optimized mobile rendering for guitar canvas

--- src/components/canvas/Guitar.jsx
+++ src/components/canvas/Guitar.jsx
@@ -4,25 +4,43 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 
 import CanvasLoader from '../Loader';
 
-const Guitar = () => {
-  const guitar = useGLTF('./gibson_sg_guitar/scene.gltf')
+const Guitar = ( { isMobile }) => {
+  const guitar = useGLTF('./gibson_sg_guitar/scene.gltf');
 
   return (
     <mesh>
       <hemisphereLight intensity={.15} groundColor="black" />
       <pointLight intensity={1} />
       <spotLight position={[10, 0, 10]} angle={90} penumbra={1} intensity={1} castShadow shadow-mapSize={1024}/>
-      <primitive object={guitar.scene} position={[0, -1, 0]} rotation={[0,0.5,0]} scale={2} />
+      <primitive object={guitar.scene} position={[0, -1, 0]} rotation={[0,0.5,0]} scale={isMobile ? 1.0 : 2.0} />
     </mesh>
   )
 }
 
 const GuitarCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 500px');
+
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    }
+
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    }
+  })
+  
   return (
     <Canvas frameloop='demand' shadows camera={{ position: [0, 0, 10], fov: 25 }} gl={{ preserveDrawingBuffer: true }}>
       <Suspense fallback={< CanvasLoader />}>
         <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
-        <Guitar />
+        <Guitar isMobile={isMobile}/>
       </Suspense>
       <Preload all />
     </Canvas>
