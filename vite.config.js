5e1fdd664295eb3f0b63f63410b696aab56cab2e
Frédéric Mheir <63975740+fredmheir@users.noreply.github.com>
2023-06-11 19:02:59 -0400

    more change in config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './index.html',
+  base: "./",
+  build: {
+    input: {
+      app: "./index.html", // default
+    },
+  },
   resolve: {
     alias: {
       "./runtimeConfig": "./runtimeConfig.browser",
