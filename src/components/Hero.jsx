9e205616c4ad52cb59f17cbf82924b8a691bb083
Frédéric Mheir <63975740+fredmheir@users.noreply.github.com>
2023-06-14 00:03:44 -0400

    Added scrolling animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -51,9 +51,17 @@ const Hero = () => {
             <TypewriterText text="I am a software developer." className={`${styles.heroSubText} mt-2 text-black-100`} />
           </p>
         </div>
-
-        
-
+      </div>
+      <div className="absolute xs:bottom-100 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-white flex justify-center items-start p-2">
+            <motion.dev 
+              animate={{ y: [0, 24, 0] }} 
+              transition={{ duration: 1.5, repeat: Infinity, repeatType: 'loop'}} 
+              className="w-3 h-2 rounded-full bg-white mb-1"
+            />
+          </div>
+        </a>
       </div>
     </section>
   )
